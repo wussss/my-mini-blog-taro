d7087991b8ff338e496419995b522e4dcd8775cc
silan.wu <silan.wu@liulishuo.com>
2020-10-27 21:25:05 +0800

    feat:todolist

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,39 @@
-import React from "react";
-import Taro, { Component } from "@tarojs/taro";
-import { View, Text } from "@tarojs/components";
+/* eslint-disable jsx-quotes */
+import React, { useState } from "react";
+import Taro from "@tarojs/taro";
+import { View, Text, Input, Button } from "@tarojs/components";
+import "./index.scss";
+import useInputEvent from "../../hooks/useInputEvent";
 
 const Index: Taro.FC = () => {
+  const [list, setList] = useState<string[]>([]);
+  const { inputValue: todo, onInputEvent, setValue: setTodo } = useInputEvent(
+    ""
+  );
+  const onSubmit = () => {
+    setList([...list, todo]);
+    setTodo("");
+  };
+
   return (
-    <View>
-      测试一下
+    <View className="index">
+      <View className="submit">
+        <Input
+          type="text"
+          placeholder="请输入todo"
+          className="input"
+          onInput={onInputEvent}
+          value={todo}
+        />
+        <Button onClick={onSubmit} className="button" size="mini">
+          Sumbit
+        </Button>
+      </View>
+      <View className="list">
+        {list.map((item, index) => (
+          <Text key={index}>{index+1}、{item}</Text>
+        ))}
+      </View>
     </View>
   );
 };
