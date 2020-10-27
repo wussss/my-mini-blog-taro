e4092aa612efbf63564275cfc738738edbc0f6c5
silan.wu <silan.wu@liulishuo.com>
2020-10-27 14:47:39 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+export default {
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#cbb8cb',
+    navigationBarTitleText: '小狗博客',
+    navigationBarTextStyle: 'white'
+  }
+}
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Taro, { Component } from "@tarojs/taro";
+import { View, Text } from "@tarojs/components";
+
+const Index: Taro.FC = () => {
+  return (
+    <View>
+      测试一下
+    </View>
+  );
+};
+
+export default Index;
