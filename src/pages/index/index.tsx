c5537064a7696c0d9f578d4d28df5611fb66e975
silan.wu <silan.wu@liulishuo.com>
2020-11-01 00:34:18 +0800

    feat:使用iconfont+增加喜欢/不喜欢功能

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -7,11 +7,18 @@ import useToggle from "../../hooks/useToggle";
 import Question from "../../components/question/question";
 
 import "./index.scss";
+import "../../statics/iconfont/iconfont.scss";
 
+interface IQuestion {
+  id: number;
+  question: String;
+  like: number;
+  unlike: number;
+}
 const Index: Taro.FC = () => {
   const { flag: isVisible, setFlag, onToggle } = useToggle(false);
   const onClose = () => setFlag(false);
-  const setStore = (key: string, data: any) => {
+  const setStore = (key: string, data: IQuestion[]) => {
     const data1 = JSON.stringify(data); //对象转成字符
     Taro.setStorageSync(key, data1);
   };
@@ -22,16 +29,47 @@ const Index: Taro.FC = () => {
     }
     return JSON.parse(data); //字符转成对象
   };
-  const [questions, setInfo] = useState(getStore("questions"));
+  const [questions, setInfo] = useState<IQuestion[]>(getStore("questions"));
   const onRecieve = (question) => {
-    setInfo([...questions, question]);
+    const questionItem: IQuestion = {
+      id: Math.random() * 10000,
+      question: question,
+      like: 0,
+      unlike: 0,
+    };
+    setInfo([...questions, questionItem]);
     Taro.showToast({ title: "问题提交成功" });
-    setStore("questions", [...questions, question]);
+    setStore("questions", [...questions, questionItem]);
   };
   const onDelete = (index: number) => {
     questions.splice(index, 1);
     setInfo([...questions]);
     setStore("questions", [...questions]);
+    console.log(questions);
+  };
+  const addCount1 = (item: IQuestion) => {
+    item.like = item.like + 1;
+    const List = questions;
+    const newList = List.map((question_item) => {
+      if (question_item.id === item.id) {
+        question_item = item;
+      }
+      return question_item;
+    });
+    setInfo([...newList]);
+    setStore("questions", [...newList]);
+  };
+  const addCount2 = (item: IQuestion) => {
+    item.unlike = item.unlike + 1;
+    const List = questions;
+    const newList = List.map((question_item) => {
+      if (question_item.id === item.id) {
+        question_item = item;
+      }
+      return question_item;
+    });
+    setInfo([...newList]);
+    setStore("questions", [...newList]);
   };
   return (
     <View className="index">
@@ -39,15 +77,36 @@ const Index: Taro.FC = () => {
       <View className="list">
         {questions.map((item, index) => {
           return (
-            <View key={index} className="list-item">
-              <Text>·</Text>
+            <View key={item.id} className="list-item">
+              <Text className="contont-before">·</Text>
               <View
-                key={index}
+                className="content"
+                key={item.id}
                 onClick={() => {
                   onDelete(index);
                 }}
               >
-                {item}
+                {item.question}
+              </View>
+              <View className="icon-list">
+                <View
+                  className="iconfont icon-weibiaoti--"
+                  style="font-size:20PX;color:#c597c5;margin-left:20PX"
+                  onClick={() => {
+                    addCount1(item);
+                  }}
+                >
+                  {item.like}
+                </View>
+                <View
+                  className="iconfont icon-dislike"
+                  style="font-size:20PX;color:#c597c5;margin-left:20PX"
+                  onClick={() => {
+                    addCount2(item);
+                  }}
+                >
+                  {item.unlike}
+                </View>
               </View>
             </View>
           );
